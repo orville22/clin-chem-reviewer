a9c965bc4f867f001a19bfc5e33cd6e0281dba3a
Orville Malijan <orville@creativiumdigital.com>
2023-06-22 17:47:15 +1000

    clin-chem v.1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>
+        Clinical Chemistry Reviewer #1 <span className="emoji">🧪</span>
+      </h1>
+      <Flashcards />
+    </div>
+  );
+}
+const questions = [
+  {
+    id: 1,
+    question: 'What are the tests included in a Liver Function Test?',
+    answer: 'ALT, AST, Bilirubin, Total Protein, Albumin, GGT',
+  },
+  {
+    id: 2,
+    question:
+      'What is the clinical significance of elevated serum creatinine levels?',
+    answer: 'Possible renal disease',
+  },
+  {
+    id: 3,
+    question: 'What is the end product of protein metabolism',
+    answer: 'Urea',
+  },
+  {
+    id: 4,
+    question: 'What is the primary lipoprotein carrying cholesterol?',
+    answer: 'LDL',
+  },
+  {
+    id: 5,
+    question: 'What is the major blood electrolyte?',
+    answer: 'Sodium (Na+)',
+  },
+  {
+    id: 6,
+    question: 'What is the main function of albumin?',
+    answer: 'Maintain osmotic pressure',
+  },
+  {
+    id: 7,
+    question: 'Give 5 common causes of artefactual hypernkalemia',
+    answer:
+      'Haemolysis, Delayed Sample Processing, Anticoagulant(commonly EDTA contamination), High platelet count, Prolonged tourniquet application',
+  },
+  {
+    id: 8,
+    question: 'What is the normal range for blood pH??',
+    answer: '7.35-7.45',
+  },
+  {
+    id: 9,
+    question: 'What enzyme breaks down starch?',
+    answer: 'Amylase',
+  },
+  {
+    id: 10,
+    question:
+      'What is the role of lipase in clinical chemistry, and what does its elevation indicate?',
+    answer:
+      'Assists in breaking down fats. Elevation indicates pancreatic damage or inflammation.',
+  },
+  {
+    id: 11,
+    question: 'Give at least 3 cardiac markers.',
+    answer: 'Troponin, CK-MB, BNP',
+  },
+  {
+    id: 12,
+    question: 'What is the principle behind immunoassay?',
+    answer:
+      'Utilizing the specificity of antigen and antibody binding to detect and quantify analytes from patient samples',
+  },
+];
+
+function Flashcards() {
+  const [selectedId, setSelectedId] = useState(null);
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() =>
+            setSelectedId(selectedId !== question.id ? question.id : null)
+          }
+          className={selectedId === question.id ? 'selected' : ''}
         >
-          Learn React
-        </a>
-      </header>
+          <p>
+            {selectedId === question.id ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
